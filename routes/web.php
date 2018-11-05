1ced75621653f33614b9ee4e2e6b1fadee0e2578
foxbloodiron <ariakbar6944@gmail.com>
2018-11-05 15:21:05 +0700

    guest

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@
 */
 
 Route::get('/', function () {
-    return redirect('home');
+    return view('home');
 });
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'guest'], function(){
 
 	Route::get('/home', 'HomeController@index')->name('home');
 
