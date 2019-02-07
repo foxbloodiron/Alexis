46e17397b26ae72b54bcafbf21f42cc693bc70c4
foxbloodiron <ariakbar6944@gmail.com>
2019-02-07 16:10:09 +0700

    Update tambah_perencanaanpengiriman.blade.php

--- resources/views/pengiriman/perencanaanpengiriman/tambah_perencanaanpengiriman.blade.php
+++ resources/views/pengiriman/perencanaanpengiriman/tambah_perencanaanpengiriman.blade.php
@@ -31,29 +31,59 @@
             </div>
             <div class="card-block">
                 <section>
-                    <div class="row">
-                      <div class="col-md-3 col-sm-6 col-12">
-                        <label>Nota</label>
-                      </div>
-
-                      <div class="col-md-3 col-sm-6 col-12">
-                        <div class="form-group">
-                          <select class="form-control form-control-sm select2">
-                            <option value="" selected="" disabled="">--Pilih--</option>
-                          </select>
+                  <fieldset>
+                      <div class="row">
+                        <div class="col-md-3 col-sm-6 col-12">
+                          <label>Nota</label>
                         </div>
-                      </div>
 
-                      <div class="col-md-3 col-sm-6 col-12">
-                        <label>Nama Customer</label>
-                      </div>
-
-                      <div class="col-md-3 col-sm-6 col-12">
-                        <div class="form-group">
-                          <input type="text " class="form-control form-control-sm" readonly="" name="">
+                        <div class="col-md-3 col-sm-6 col-12">
+                          <div class="form-group">
+                            <select class="form-control form-control-sm select2" id="nota" name="nota">
+                              <option value="" selected="" disabled="">--Pilih--</option>
+                              <option value="1">POS-TO/20190207/1</option>
+                            </select>
+                          </div>
                         </div>
+
+                        <div class="col-md-3 col-sm-6 col-12">
+                          <label>Nama Customer</label>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-12">
+                          <div class="form-group">
+                            <input type="text " class="form-control form-control-sm" readonly="" value="Alpha" id="customer" name="customer">
+                          </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-12">
+                          <label>Alamat</label>
+                        </div>
+
+                        <div class="col-md-9 col-sm-6 col-12">
+                          <div class="form-group">
+                            <textarea class="form-control" readonly="" name=""></textarea>
+                          </div>
+                        </div>
+
+
                       </div>
-                    </div>
+                  </fieldset>
+                  <div class="mt-3 table-responsive">
+                    <table class="table table-bordered table-striped table-hover" id="table_listbarang" cellspacing="0">
+                      <thead class="bg-primary">
+                        <tr>
+                          <th>Barang</th>
+                          <th>Satuan</th>
+                          <th>Qty</th>
+                          <th>Pemilik Kendaraan</th>
+                          <th>Nama Pemilik</th>
+                          <th>Nopol</th>
+                          <th>Sopir</th>
+                        </tr>
+                      </thead>
+                    </table>
+                  </div>
                 </section>
             </div>
             <div class="card-footer text-right">
@@ -75,7 +105,136 @@
 @section('extra_script')
 <script type="text/javascript">
   $(document).ready(function(){
-    
+    var eueue = $('#table_listbarang').DataTable();
+
+    var counter = 0;
+
+    $('#nota').change(function(){
+
+      var ini = $(this).val();
+
+      eueue.row.add([
+        'Paving Abu',
+        'PCS',
+        '100',
+        '<select class="form-control form-control-sm pemilik_kendaraan">'+
+          '<option value="">--Pilih--</option>'+
+          '<option value="1">Alexis</option>'+
+          '<option value="2">Customer</option>'+
+        '</select>',
+        '<input type="text" class="form-control form-control-sm nama_pemilik" readonly="">',
+        '<select class="form-control form-control-sm pilih_nopol" readonly="">'+          
+        '</select>',
+        '<input type="text" class="form-control form-control-sm nama_sopir" readonly="">',
+
+
+      ]).draw();
+
+      eueue.row.add([
+        'Paving Merah',
+        'PCS',
+        '120',
+        '<select class="form-control form-control-sm pemilik_kendaraan">'+
+          '<option value="">--Pilih--</option>'+
+          '<option value="1">Alexis</option>'+
+          '<option value="2">Customer</option>'+
+        '</select>',
+        '<input type="text" class="form-control form-control-sm nama_pemilik" readonly="">',
+        '<select class="form-control form-control-sm pilih_nopol" readonly="">'+          
+        '</select>',
+        '<input type="text" class="form-control form-control-sm nama_sopir" readonly="">',
+
+
+      ]).draw();
+
+      eueue.row.add([
+        'Paving 8T',
+        'PCS',
+        '50',
+        '<select class="form-control form-control-sm pemilik_kendaraan">'+
+          '<option value="">--Pilih--</option>'+
+          '<option value="1">Alexis</option>'+
+          '<option value="2">Customer</option>'+
+        '</select>',
+        '<input type="text" class="form-control form-control-sm nama_pemilik" readonly="">',
+        '<select class="form-control form-control-sm pilih_nopol" readonly="">'+          
+        '</select>',
+        '<input type="text" class="form-control form-control-sm nama_sopir" readonly="">',
+
+
+      ]).draw();      
+
+
+      counter++;
+    });
+
+
+    $('#table_listbarang tbody').on('change', '.pemilik_kendaraan', function(){
+
+      var ini, pemilik_kendaraan, nama_pemilik, pilih_nopol, tuwek, nama_sopir;
+
+      ini = $(this);
+      tuwek = ini.parents('tr');
+      
+      nama_pemilik = tuwek.find('.nama_pemilik');
+      pilih_nopol = tuwek.find('.pilih_nopol');
+      nama_sopir = tuwek.find('.nama_sopir');
+      
+
+      if (ini.val() === '1') {
+        nama_pemilik.val('CV. Alexis');
+        pilih_nopol.attr('readonly', false);
+        pilih_nopol.html(
+          '<option value="">--Pilih--</option>'+
+          '<option value="1">L 1234 UD</option>'+
+          '<option value="2">L 4321 BV</option>');
+        nama_sopir.val('');
+      } else if(ini.val() === '2'){
+        nama_pemilik.val('Alpha');
+        pilih_nopol.attr('readonly', false);
+        pilih_nopol.html(
+          '<option value="">--Pilih--</option>'+
+          '<option value="1">L 6789 UD</option>'+
+          '<option value="2">L 9876 BV</option>');
+        nama_sopir.val('');
+      } else {
+        nama_pemilik.val('');
+        pilih_nopol.attr('readonly', true);
+        pilih_nopol.html('');
+        nama_sopir.val('');
+
+      }
+
+    });
+
+    $('#table_listbarang tbody').on('change', '.pilih_nopol', function(){
+
+      var ini, tuwek, nama_sopir, pemilik_kendaraan;
+
+      ini = $(this);
+      tuwek = ini.parents('tr');
+
+      nama_sopir  = tuwek.find('.nama_sopir');
+      pemilik_kendaraan  = tuwek.find('.pemilik_kendaraan');
+
+      if (pemilik_kendaraan.val() === '1') {
+        if (ini.val() === '1') {
+          nama_sopir.val('Su Ep');
+        } else if(ini.val() === '2'){
+          nama_sopir.val('Tole');
+        } else {
+          nama_sopir.val('');
+        }
+      } else if(pemilik_kendaraan.val() === '2'){
+        if (ini.val() === '1') {
+          nama_sopir.val('Paijo');
+        } else if(ini.val() === '2'){
+          nama_sopir.val('Wawan');
+        } else {
+          nama_sopir.val('');
+        }
+      }
+    });
   });
 </script>
 @endsection
